811a00f75255b5d3bbb7053521f0e0b55a0beab4
Matheus Amorim <mtsgamorim@gmail.com>
2022-07-07 18:43:44 -0300

    Solving Conflicts

--- src/components/HomePage/TypeBookSeparation.js
+++ src/components/HomePage/TypeBookSeparation.js
@@ -24,13 +24,9 @@ export default function TypeBookSeparation({ type }) {
   }
 
   useEffect(() => {
-<<<<<<< HEAD
     const promise = axios.get(
       `https://store-bookstore.herokuapp.com/products/${type}`
     );
-=======
-    const promise = axios.get(`https://store-bookstore.herokuapp.com/products/${type}`);
->>>>>>> d3d3749be79e1897d0c827c2af7adc8cc80c0fe2
 
     promise.then((res) => {
       setBooks(res.data);
@@ -45,23 +41,21 @@ export default function TypeBookSeparation({ type }) {
       <h1>{type}:</h1>
 
       <BookSpace>
-
-        {(books.length>0)
-        ?
-        books.map((book, index) => (
-          <IndividualBook
-            key={index}
-            id={book.id}
-            name={book.name}
-            image={book.image}
-            type2={type}
-            price={book.price}
-          />
-        ))
-        :
-        <h6>Não há livros para exibir!</h6> 
-      }
-      {}
+        {books.length > 0 ? (
+          books.map((book, index) => (
+            <IndividualBook
+              key={index}
+              id={book.id}
+              name={book.name}
+              image={book.image}
+              type2={type}
+              price={book.price}
+            />
+          ))
+        ) : (
+          <h6>Não há livros para exibir!</h6>
+        )}
+        {}
       </BookSpace>
     </Container>
   );
@@ -74,14 +68,14 @@ const Container = styled.div`
   align-items: flex-start;
   justify-content: center;
   width: 860px;
-  height:fit-content;
-  flex-wrap:wrap;
-  font-family: 'Bungee Shade', cursive;
-  color:#fbefdc;
-  background-color:#424e5e;
-  padding:25px 0 50px 0;
-  border-radius:8px;
-  
+  height: fit-content;
+  flex-wrap: wrap;
+  font-family: "Bungee Shade", cursive;
+  color: #fbefdc;
+  background-color: #424e5e;
+  padding: 25px 0 50px 0;
+  border-radius: 8px;
+
   h1 {
     font-weight: 700;
     font-size: 30px;
@@ -91,15 +85,15 @@ const Container = styled.div`
 
 const BookSpace = styled.div`
   display: flex;
-  align-items:center;
-  justify-content:space-between;
+  align-items: center;
+  justify-content: space-between;
   overflow: hidden;
   /* width: 70%; */
-  width:fit-content;
+  width: fit-content;
   /* min-width: 230px; */
-  max-width:710px;
-  flex-wrap:wrap;
-  height:fit-content;
+  max-width: 710px;
+  flex-wrap: wrap;
+  height: fit-content;
   margin-left: auto;
   margin-right: auto;
   border: 1px solid #424e5e;
@@ -107,13 +101,13 @@ const BookSpace = styled.div`
   border-radius: 5px;
   background-color: var(--color-bg-footer);
   font-family: "Roboto", sans-serif;
-  margin-top:20px;
-  padding:20px 20px 0 20px;
-  h6{
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    padding-bottom:15px;
+  margin-top: 20px;
+  padding: 20px 20px 0 20px;
+  h6 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 15px;
   }
 `;
 
@@ -123,7 +117,7 @@ const Book = styled.div`
   align-items: center;
   flex-direction: column;
   color: #000f27;
-  margin-bottom:20px;
+  margin-bottom: 20px;
   img {
     width: 200px;
     height: 260px;
@@ -135,13 +129,13 @@ const Book = styled.div`
 
 const AdjustWidth = styled.div`
   width: 200px;
-  padding:5px;
-  display:flex;
-  flex-direction:column;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
   align-items: flex-start;
-  justify-content:center;
-  h3{
-    margin-bottom:5px;
-    flex-wrap:wrap;
+  justify-content: center;
+  h3 {
+    margin-bottom: 5px;
+    flex-wrap: wrap;
   }
 `;
