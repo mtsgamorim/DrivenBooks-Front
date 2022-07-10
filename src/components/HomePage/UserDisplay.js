65c6d2c9e2fca641b5e838a8395b9dc019415fa0
Dayane <dayanecarolina.oliveira@gmail.com>
2022-07-10 20:59:19 -0300

    Solving bugs layout

--- src/components/HomePage/UserDisplay.js
+++ src/components/HomePage/UserDisplay.js
@@ -9,7 +9,11 @@ export default function UserDisplay() {
   return (
     <DisplayName>
       <div>
-        <span>
+        <span className="Desktop">
+          <div>Bem vindo,</div>
+          <div>{userData.name}</div> 
+        </span>
+        <span className="Mobile">
           <div>Bem vindo,</div>
           <div>{userData.name}</div> 
         </span>
@@ -26,26 +30,43 @@ const DisplayName = styled.div`
   align-items: center;
   justify-content: space-between;
   margin-right: 80px;
-  span {
-    font-size: 28px;
+  /* span {
+    font-size: 26px;
     margin-left:80px;
+  } */
+  .Mobile{
+    display:none;
+  }
+    
   }
   ion-icon {
     font-size: 30px;
     margin-left: 18px;
   }
   @media (max-width: 767px) {
+    .Desktop{
+      display:none;
+    }
+    .Mobile{
+      display:initial;
+      display:inline-block;
+    }
+    div{
     width:100%;
     margin:0px;
     padding-left:40px;
-    div:last-child{
-      padding-bottom:20px;
+    div:first-child{
+      display:flex;
+      flex-direction:row;
     }
+    div:last-child{
+      padding-bottom:10x;
+    }
+    
     span{
       font-size:16px;
-      flex-wrap:wrap;
       max-height:100%;
-      text-align:center;
+      text-align:start;
     }
   }
 `;
