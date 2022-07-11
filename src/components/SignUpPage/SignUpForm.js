e655cb6b9d72da96d8d8cfc2148563d2c6b05107
Dayane <dayanecarolina.oliveira@gmail.com>
2022-07-10 21:10:13 -0300

    Disable button deleted

--- src/components/SignUpPage/SignUpForm.js
+++ src/components/SignUpPage/SignUpForm.js
@@ -14,7 +14,7 @@ export default function SignUpForm(){
     let [password, setPassword] = useState("");
     let [confirmPassword, setConfirmPassword] = useState("");
     const [loading,setLoading] = useState(false);
-    let isClicked = false;
+
 
     async function submitData(event) {
         event.preventDefault();
@@ -42,24 +42,23 @@ export default function SignUpForm(){
     }
 
     function loadingButton(){
-        isClicked=!isClicked;
         return loading ? 
             (<ThreeDots color="#FFF" background-color={"var(--color-bg-input)"} opacity={0.7} height={80} width={80} />)
             :
             ("CADASTRAR");
     }
 
-    function disable(){
-        return isClicked ? "disable" : "";
-    }
+    // function disable(){
+    //     return isClicked ? "disable" : "";
+    // }
 
     return(
         
         <form onSubmit={submitData}>
-            <Input type="text" placeholder="Nome" value={name} onChange={e=> setName(e.target.value)} disabled={disable()} color={loading} maxLength={12} required  />
-            <Input type="email" placeholder="E-mail" value={email} onChange={e=> setEmail(e.target.value)} disabled={disable()} color={loading} required  />
-            <Input type="password" placeholder="Senha" value={password} onChange={e=> setPassword(e.target.value)} disabled={disable()} color={loading} required  />
-            <Input type="password" placeholder="Confirme a senha" value={confirmPassword} onChange={e=> setConfirmPassword(e.target.value)} disabled={disable()} color={loading} required  />
+            <Input type="text" placeholder="Nome" value={name} onChange={e=> setName(e.target.value)}  color={loading} maxLength={12} required  />
+            <Input type="email" placeholder="E-mail" value={email} onChange={e=> setEmail(e.target.value)}  color={loading} required  />
+            <Input type="password" placeholder="Senha" value={password} onChange={e=> setPassword(e.target.value)}  color={loading} required  />
+            <Input type="password" placeholder="Confirme a senha" value={confirmPassword} onChange={e=> setConfirmPassword(e.target.value)}  color={loading} required  />
             <Button>{loadingButton()}</Button>
         </form>
         
